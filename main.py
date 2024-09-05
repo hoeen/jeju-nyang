9a1dea5cb9aa94828282b8bd46bc8b12a6e5b40d
hoeen <hoeen5373@gmail.com>
2024-09-05 23:50:32 +0900

    pysqlite3 위치변경

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from exec_streamlit.chatbox_gpt_light import main
-
-import pysqlite3
+__import__('pysqlite3')
 import sys
-
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+from exec_streamlit.chatbox_gpt_light import main
 
 if __name__ == "__main__":
     main()
